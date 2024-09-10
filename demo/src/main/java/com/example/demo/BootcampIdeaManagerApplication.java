bc83c60b669f83e01740c30a59ef4c138cc05cc6
Ilya Avgul <ilia.nekto@gmail.com>
2024-09-10 11:58:22 +0200

    setting up database connectivity

--- demo/src/main/java/com/example/demo/BootcampIdeaManagerApplication.java
+++ demo/src/main/java/com/example/demo/BootcampIdeaManagerApplication.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampIdeaManagerApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BootcampIdeaManagerApplication.class, args);
 	}
-
 }
