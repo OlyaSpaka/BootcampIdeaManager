34b914419c0221eb477a59319c883e6ea9e52c97
Marija Buivyte <70386611+marijaab@users.noreply.github.com>
2024-09-17 20:19:55 +0300

    clean up the code

--- demo/src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -1,14 +1,12 @@
 package com.example.demo.controllers;
 
-import com.example.demo.exceptions.EmailTakenException;
-import com.example.demo.exceptions.UsernameTakenException;
-import com.example.demo.models.User;
+import com.example.demo.dto.RegisterDTO;
 import com.example.demo.services.AuthenticationService;
 import jakarta.validation.Valid;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,29 +29,37 @@ public class AuthenticationController {
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         if (!model.containsAttribute("user")){
-            model.addAttribute("user", new User());
+            model.addAttribute("user", new RegisterDTO());
         }
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result,
+    public String registerUser(@Valid @ModelAttribute("user") RegisterDTO registerDTO, BindingResult result,
                                RedirectAttributes redirectAttributes) {
+
+        if(authenticationService.isUsernameTaken(registerDTO.getUsername())) {
+            result.addError(new FieldError("user", "username", "Username already in use.")); }
+
+        if(authenticationService.isEmailTaken(registerDTO.getEmail())) {
+            result.addError(new FieldError("user", "email", "Email already in use.")); }
+
         if (result.hasErrors()) {
             redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", result);
-            redirectAttributes.addFlashAttribute("user", user);
+            redirectAttributes.addFlashAttribute("user", registerDTO);
             return "redirect:/auth/register"; // Return to form with validation errors
         }
+
         try {
-            authenticationService.registerNewUser(user);
-            redirectAttributes.addFlashAttribute("success", "Registration successful!");
-            return "redirect:/auth/login";
-        }  catch (UsernameTakenException e) {
-            redirectAttributes.addFlashAttribute("usernameError", e.getMessage());
-        } catch (EmailTakenException e) {
-            redirectAttributes.addFlashAttribute("emailError", e.getMessage());
+            authenticationService.registerNewUser(registerDTO);
+            redirectAttributes.addFlashAttribute("success", "Registration was successful!");
+            return "redirect:/auth/register";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("unspecifiederror", "Something went wrong. Try again later.");
         }
-        redirectAttributes.addFlashAttribute("user", user);
+
+        redirectAttributes.addFlashAttribute("user", registerDTO);
         return "redirect:/auth/register";
+
     }
 }
--- demo/src/main/java/com/example/demo/services/AuthenticationService.java
+++ demo/src/main/java/com/example/demo/services/AuthenticationService.java
@@ -1,23 +1,18 @@
 package com.example.demo.services;
 
-import com.example.demo.exceptions.EmailTakenException;
-import com.example.demo.exceptions.UsernameTakenException;
+import com.example.demo.dto.RegisterDTO;
 import com.example.demo.models.Role;
 import com.example.demo.models.User;
 import com.example.demo.repositories.RoleRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class AuthenticationService implements UserDetailsService {
@@ -37,36 +32,18 @@ public class AuthenticationService implements UserDetailsService {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found: " + username));
     }
 
-    private Collection<GrantedAuthority> mapRolesToAuthorities(Set<Role> roles) {
-        return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName())).collect(Collectors.toSet());
-    }
-
-
-    private boolean isUsernameTaken(String username) {
+    public boolean isUsernameTaken(String username) {
         return userRepository.findByUsername(username).isPresent();
     }
 
-    private boolean isEmailTaken(String email) {
+    public boolean isEmailTaken(String email) {
         return userRepository.findByEmail(email).isPresent();
     }
 
-    public void registerNewUser(User user) throws UsernameTakenException, EmailTakenException {
-        System.out.println("Saving user: " + user.getUsername());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
+    public void registerNewUser(RegisterDTO registerDTO) {
+        User user = new User(registerDTO.getUsername(), registerDTO.getEmail(), passwordEncoder.encode(registerDTO.getPassword()));
         Role roles = roleRepository.findByName("User").get();
         user.setRoles(Set.of(roles));
-
-        if (isUsernameTaken(user.getUsername())) {
-            System.out.println("Username already exists");
-            throw new UsernameTakenException("Username already exists");
-        }
-
-        if (isEmailTaken(user.getEmail())) {
-            System.out.println("Email already exists");
-            throw new EmailTakenException("Email already exists");
-        }
-
         userRepository.save(user);
     }
     public User getCurrentUser() {
