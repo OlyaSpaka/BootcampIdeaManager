4d5e598addd91e7bcfb0a6c7aa83190b61bd6b62
Olga <shpakaolga@gmail.com>
2024-09-18 00:16:49 +0300

    Added Bookmark functionality:  1. visible bookmarks  2. ability to add and delete bookmark on /ideas and on /ideas/id pages  3. filter by bookmarked ideas  4. reworked BookmarkServiceTest

--- demo/src/main/java/com/example/demo/repositories/BookmarkRepository.java
+++ demo/src/main/java/com/example/demo/repositories/BookmarkRepository.java
@@ -1,19 +1,13 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.Bookmark;
-import com.example.demo.models.Idea;
-import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
     List<Bookmark> findByUserId(Integer userId);
-
-    Optional<Bookmark> findByIdeaAndUser(Idea idea, User user);
-
     void deleteByIdeaIdAndUserId(Integer ideaId, Integer userId);
 }
